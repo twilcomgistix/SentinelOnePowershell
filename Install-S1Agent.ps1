c2bbd982b264e71f214f77ead115d72ff7495297
Ted Williams <84555242+twilcomgistix@users.noreply.github.com>
2022-11-19 22:07:26 -0600

    Update Install-S1Agent.ps1
    Enhanced the script with a parameter block. Required variables are now mandatory for execution.
    
    Wrapped the

--- Install-S1Agent.ps1
+++ Install-S1Agent.ps1
@@ -1,33 +1,37 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)][String]$Server,
+    [Parameter(Mandatory)][String]$SiteToken,
+    [Parameter(Mandatory)][String]$ApiToken
+)
+
 # Force TLS 1.2. Not always necessary but Windows Version below 1903 will default to TLS 1.1 or worse and fail.
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
-# Set the hostname of your server here.
-$Server = "Your-Server"
-# Set the site token for the Sentinel One site here
-$siteToken = "Your-Site-Token-Here"
-# Set your API Token here
-$ApiToken = "Your-API-Token-Here"
+Try {
+    # Set headers with API Token
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", "ApiToken $ApiToken")
+    $headers.Add("Content-Type", "application/json")
 
-# Set headers with API Token
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", "ApiToken $ApiToken")
-$headers.Add("Content-Type", "application/json")
+    # Get the details on the latest General Availability MSI installer available from your Sentinel One server instance.
+    $response = Invoke-RestMethod "https://$Server.sentinelone.net/web/api/v2.1/update/agent/packages?platformTypes=windows&status=ga&fileExtension=.msi&sortOrder=desc&limit=2" -Method 'GET' -Headers $headers
+    $payload = $response.data | Where {
+        $_.osArch -eq "64 bit"
+    }
+    # Set the filename and location for the downloaded installer
+    $file = "C:\SentinelAgent_windows.msi"
 
-# Get the details on the latest General Availability MSI installer available from your Sentinel One server instance.
-$response = Invoke-RestMethod "https://$Server.sentinelone.net/web/api/v2.1/update/agent/packages?platformTypes=windows&status=ga&fileExtension=.msi&sortOrder=desc&limit=2" -Method 'GET' -Headers $headers
-$payload = $response.data | Where {
-    $_.osArch -eq "64 bit"
-}
+    # Download the latest 64-Bit MSI Installer.
+    Invoke-WebRequest -Uri $payload.link -Outfile $file -Headers $headers -UseBasicParsing
 
-# Set the filename and location for the downloaded installer
-$file = "C:\SentinelAgent_windows.msi"
-
-# Download the latest 64-Bit MSI Installer.
-Invoke-WebRequest -Uri $payload.link -Outfile $file -Headers $headers -UseBasicParsing
-
-# Silently install the agent and set the site token. No restart.
-if ($file) {
-    Start-Process msiexec.exe -ArgumentList "/i $file SITE_TOKEN=$siteToken /q /quiet /norestart"
-} else {
-    Write-Host "Could not find $file; Did it fail to download?" -ForegroundColor Red
+    # Silently install the agent and set the site token. No restart.
+    if (Test-Path $file) {
+        Start-Process msiexec.exe -ArgumentList "/i $file SITE_TOKEN=$SiteToken /q /quiet /norestart"
+    } else {
+        Write-Host "Could not find $file; Did it fail to download?" -ForegroundColor Red
+    }
+} Catch {
+    $Err = $Error[0].Exception.Message
+    Write-Host $Err -ForegroundColor Red
 }
